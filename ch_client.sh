734fa779703d98ec5c2527c20b71b58cc4adeabd
Zaytsev Dmitriy <zaytsev_dmitriy@edadeal.ru>
2018-12-06 03:24:12 +0700

    fix typo

--- ch_client.sh
+++ ch_client.sh
@@ -11,28 +11,28 @@ select choice in \
   "Shard 3 Replica 2"; 
 do
 case "$choice" in
-    "Shard 1 Replica 1") echo "Connecting to $choice ..."; docker container run -it --rm --network chcompose_main \
-       --link chcompose_clickhouse_shard_1_replica_1_1:clickhouse-server yandex/clickhouse-client \
+    "Shard 1 Replica 1") echo "Connecting to $choice ..."; docker container run -it --rm --network ch_compose_main \
+       --link ch_compose_clickhouse_shard_1_replica_1_1:clickhouse-server yandex/clickhouse-client \
        --host clickhouse-server 
     break;;
-    "Shard 1 Replica 2") echo "Connecting to $choice ..."; docker container run -it --rm --network chcompose_main \
-       --link chcompose_clickhouse_shard_1_replica_2_1:clickhouse-server yandex/clickhouse-client \
+    "Shard 1 Replica 2") echo "Connecting to $choice ..."; docker container run -it --rm --network ch_compose_main \
+       --link ch_compose_clickhouse_shard_1_replica_2_1:clickhouse-server yandex/clickhouse-client \
        --host clickhouse-server 
     break;;
-    "Shard 2 Replica 1") echo "Connecting to $choice ..."; docker container run -it --rm --network chcompose_main \
-       --link chcompose_clickhouse_shard_2_replica_1_1:clickhouse-server yandex/clickhouse-client \
+    "Shard 2 Replica 1") echo "Connecting to $choice ..."; docker container run -it --rm --network ch_compose_main \
+       --link ch_compose_clickhouse_shard_2_replica_1_1:clickhouse-server yandex/clickhouse-client \
        --host clickhouse-server 
     break;;
-    "Shard 2 Replica 2") echo "Connecting to $choice ..."; docker container run -it --rm --network chcompose_main \
-       --link chcompose_clickhouse_shard_2_replica_2_1:clickhouse-server yandex/clickhouse-client \
+    "Shard 2 Replica 2") echo "Connecting to $choice ..."; docker container run -it --rm --network ch_compose_main \
+       --link ch_compose_clickhouse_shard_2_replica_2_1:clickhouse-server yandex/clickhouse-client \
        --host clickhouse-server 
     break;;
-    "Shard 3 Replica 1") echo "Connecting to $choice ..."; docker container run -it --rm --network chcompose_main \
-       --link chcompose_clickhouse_shard_3_replica_1_1:clickhouse-server yandex/clickhouse-client \
+    "Shard 3 Replica 1") echo "Connecting to $choice ..."; docker container run -it --rm --network ch_compose_main \
+       --link ch_compose_clickhouse_shard_3_replica_1_1:clickhouse-server yandex/clickhouse-client \
        --host clickhouse-server 
     break;;
-    "Shard 3 Replica 2") echo "Connecting to $choice ..."; docker container run -it --rm --network chcompose_main \
-       --link chcompose_clickhouse_shard_3_replica_2_1:clickhouse-server yandex/clickhouse-client \
+    "Shard 3 Replica 2") echo "Connecting to $choice ..."; docker container run -it --rm --network ch_compose_main \
+       --link ch_compose_clickhouse_shard_3_replica_2_1:clickhouse-server yandex/clickhouse-client \
        --host clickhouse-server 
     break;;
     *) echo "Error: Please try again (select 1..6)!"
